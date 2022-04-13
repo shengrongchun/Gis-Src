e4712bde8bd3587a486fee9c77a06c714cf59f4c
shengrongchun <1010449768@qq.com>
2022-04-13 23:54:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Container msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Container from './components/container.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style>
+html,body,#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">hello</div>
+</template>
+<script>
+export default {
+  name: 'gis-container',
+  setup() {
+    return {}
+  }
+}
+</script>
+<style scoped lang="less">
+.container {
+  background: #eee;
+  height: 100%;
+}
+</style>
